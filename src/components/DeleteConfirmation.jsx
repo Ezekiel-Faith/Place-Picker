456d11abd81e60dd5b914391d8d96d3c63cbe89e
Ezekie-Faith <ezekielfaith2000@gmail.com>
2025-04-07 11:05:36 +0100

    using useEffect to clean up the timer

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    console.log('SET TIMER');
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    return () => {
+      console.log('cleaning up');
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <div id='delete-confirmation'>
       <h2>Are you sure?</h2>
